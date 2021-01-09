caf099484b94e43e3d6c928e0130417f9ff14f0a
Gaurav Dagade <gauravdagde@users.noreply.github.com>
2021-01-09 23:01:59 +0530

    Update admin.py

--- brownie/interview_request/admin.py
+++ brownie/interview_request/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from brownie.interview_request.models import InterviewRequestResult, Users, Company, JobProfile, InterviewRequest, \
+    TypeformWebhookData
+
+admin.register(Users)
+admin.register(Company)
+admin.register(JobProfile)
+admin.register(InterviewRequest)
+admin.register(InterviewRequestResult)
+admin.register(TypeformWebhookData)
