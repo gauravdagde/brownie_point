fd83fdfdd409fb49036a0bdf20e4d6d77963e1ef
Gaurav Dagade <gauravdagde@users.noreply.github.com>
2021-01-11 00:54:47 +0530

    Update scheduler.py

--- brownie/utils/scheduler.py
+++ brownie/utils/scheduler.py
@@ -5,5 +5,5 @@ from brownie.utils import cron
 
 def start():
     scheduler = BackgroundScheduler()
-    scheduler.add_job(cron.schedule_execute_interview_request, 'interval', minutes=60)
+    scheduler.add_job(cron.schedule_execute_interview_request, 'interval', minutes=5)
     scheduler.start()
