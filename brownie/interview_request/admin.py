3c0dc906046e9680548666f2e992b5d5f2993618
Gaurav Dagade <gauravdagde@users.noreply.github.com>
2021-01-11 01:13:07 +0530

    Update admin.py

--- brownie/interview_request/admin.py
+++ brownie/interview_request/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 
-# Register your models here.
 from brownie.interview_request.models import InterviewRequestResult, User, Company, JobProfile, InterviewRequest, \
     TypeformWebhookData
 
+admin.site.site_header = 'Interview Brownie Point'
 admin.site.register(User)
 admin.site.register(Company)
 admin.site.register(JobProfile)
